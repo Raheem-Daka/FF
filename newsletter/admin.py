85081a6c806c905c4f8030cd441426d497c71f0f
Raheem Daka <walimbadakar@gmail.com>
2026-08-01 21:45:08 +0200

    admin.py commit

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,109 @@
 from django.contrib import admin
+from .models import (
+    Subscriber,
+    UserInterest,
+    NewsletterCampaign,
+    ProductInteraction,
+)
 
-# Register your models here.
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "user",
+        "verified",
+        "active",
+        "subscribed_at",
+    )
+
+    list_filter = (
+        "verified",
+        "active",
+        "subscribed_at",
+    )
+
+    search_fields = (
+        "email",
+        "user__username",
+    )
+
+    readonly_fields = (
+        "token",
+        "token_created_at",
+        "subscribed_at",
+    )
+
+    ordering = ("-subscribed_at",)
+
+
+@admin.register(UserInterest)
+class UserInterestAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "item",
+        "views",
+        "clicks",
+        "updated_at",
+    )
+
+    list_filter = (
+        "updated_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "item__name",
+    )
+
+    readonly_fields = (
+        "updated_at",
+    )
+
+    ordering = ("-updated_at",)
+
+
+@admin.register(NewsletterCampaign)
+class NewsletterCampaignAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "subject",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "subject",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
+
+
+@admin.register(ProductInteraction)
+class ProductInteractionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "item",
+        "views",
+        "clicks",
+        "updated_at",
+    )
+
+    list_filter = (
+        "updated_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "item__name",
+    )
+
+    readonly_fields = (
+        "updated_at",
+    )
+
+    ordering = ("-updated_at",)
--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from .models import UserSession, User2FA
 
-# Register your models here.
+
+@admin.register(UserSession)
+class UserSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "device",
+        "ip_address",
+        "last_active",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "last_active",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "device",
+        "ip_address",
+    )
+
+    readonly_fields = (
+        "user",
+        "session_key",
+        "device",
+        "ip_address",
+        "user_agent",
+        "last_active",
+        "created_at",
+    )
+
+    ordering = ("-last_active",)
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(User2FA)
+class User2FAAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "is_enabled",
+    )
+
+    list_filter = (
+        "is_enabled",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+
+    readonly_fields = (
+        "user",
+        "secret",
+    )
+
+    ordering = ("user",)
